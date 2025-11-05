3668319189bbe92acf6873fbdd398320c4178e24
conor-wilson <conorwilson1998@gmail.com>
2025-11-05 23:35:18 +1100

    Added smoother wave effect

--- crowd_member.gd
+++ crowd_member.gd
@@ -1,14 +1,18 @@
 class_name CrowdMember extends Node2D
 
-@onready var held_sign: Label = $HeldSign
+signal exited_screen(CrowdMember)
+
+@onready var held_sign: ColorRect = $HeldSign
+@onready var held_sign_label: Label = $HeldSign/Label
 @onready var standup_timer: Timer = $StandupTimer
 
 @export var has_sign:bool = false
+@export var letter:String = ""
 
 @export var camera:Camera2D
 
 var sitting_pos_y:float
-const STANDING_DIFF:float = -32
+const STANDING_DIFF:float = -16
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,10 +20,14 @@ func _ready() -> void:
 
 func reset():
 	
-	if !has_sign:
+	if has_sign:
+		held_sign_label.text = letter
+		held_sign.show()
+	else:
 		held_sign.hide()
 	
 	sitting_pos_y = position.y
+
 
 func _process(delta: float) -> void:
 	
@@ -29,16 +37,16 @@ func _process(delta: float) -> void:
 func stand_up():
 	var tween = create_tween()
 	tween.tween_property(self, "position", Vector2(position.x, sitting_pos_y + STANDING_DIFF), 0.1)
-	#position.y = sitting_pos_y + STANDING_DIFF
 	standup_timer.start()
 
 func sit_down():
 	var tween = create_tween()
 	tween.tween_property(self, "position", Vector2(position.x, sitting_pos_y), 0.1)
-	#position.y = sitting_pos_y
 
-#func _on_area_2d_area_entered(area: Area2D) -> void:
-	#stand_up()
 
 func _on_standup_timer_timeout() -> void:
 	sit_down()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	exited_screen.emit(self)
--- endless_runner/crowd_row.gd
+++ endless_runner/crowd_row.gd
@@ -1,14 +1,58 @@
 extends Parallax2D
 
 @export var camera:Camera2D
+@export var first_member_offset:float
+
+@export var crowd_member_scene:PackedScene
+@export var spacing_between_crowd_members:int = 48+8
+@export var num_crowd_members:int = 5
+
+var spawn_buffer:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	reset()
+	
+
+func reset():
+	
+	# Clear out any existing crowd members
 	for child in get_children():
 		if child is CrowdMember:
-			child.camera = camera
+			child.queue_free()
+	#crowd_members = []
+	#current_crowd_member_index = -1
+	
+	# Setup the crowd members
+	spawn_buffer = first_member_offset
+	for letter in range(0, num_crowd_members):
+		_spawn_new_crowd_member()
+	
+	# Enable waving
+	#allowed_to_wave = true
+	
+	#for child in get_children():
+		#if child is CrowdMember:
+			#child.camera = camera
+
+func _spawn_new_crowd_member():
+	var new_crowd_member = crowd_member_scene.instantiate() as CrowdMember
+	add_child(new_crowd_member)
+	new_crowd_member.camera = camera
+	new_crowd_member.position = Vector2(spawn_buffer, 0)
+	new_crowd_member.exited_screen.connect(_on_crowd_member_exited_screen)
+	#new_crowd_member.letter = letter
+	new_crowd_member.reset()
+	#crowd_members.append(new_crowd_member)
+	spawn_buffer += spacing_between_crowd_members
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_crowd_member_exited_screen(crowd_member:CrowdMember):
+	crowd_member.queue_free()
+	_spawn_new_crowd_member()
