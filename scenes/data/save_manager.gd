d74fec5d6b06915bab29ae4ec340ba6e617a5afa
Clicksurfer <amit.chameides@gmail.com>
2025-11-18 02:21:33 +1100

    Cleaned up SaveManager, made it autoload.

--- scenes/data/save_manager.gd
+++ scenes/data/save_manager.gd
@@ -1,16 +1,19 @@
-class_name SaveManager
+extends Node
 
 const FILE_PATH := "user://save.json"
 
 var data:Dictionary
 
+func _init() -> void:
+	_load_game()
+	
 func set_value(key: String, value) -> void:
 	var mark_dirty = false
 	if value != get_value(key):
 		mark_dirty = true # Needs to be set prior to set
 	data[key] = value
 	if mark_dirty:
-		save_game()
+		_save_game()
 
 func get_value(key: String, default = null):
 	return data.get(key, default)
@@ -18,14 +21,14 @@ func get_value(key: String, default = null):
 func remove_value(key: String) -> void:
 	if data.has(key):
 		data.erase(key)
-		save_game()
+		_save_game()
 
-func save_game() -> void:
+func _save_game() -> void:
 	var file := FileAccess.open(FILE_PATH, FileAccess.WRITE)
 	file.store_string(JSON.stringify(data))
 
-func load_game() -> Dictionary:
+func _load_game() -> void:
 	if not FileAccess.file_exists(FILE_PATH):
-		return {}
+		return
 	var file := FileAccess.open(FILE_PATH, FileAccess.READ)
-	return JSON.parse_string(file.get_as_text()) as Dictionary
+	data = JSON.parse_string(file.get_as_text()) as Dictionary
