505cfc58508707d1e03fdd4a3b5646fa071f50d7
Clicksurfer <amit.chameides@gmail.com>
2025-11-18 01:55:37 +1100

    Created save manager class

---
+++ scenes/data/save_manager.gd
@@ -0,0 +1,31 @@
+class_name SaveManager
+
+const FILE_PATH := "user://save.json"
+
+var data:Dictionary
+
+func set_value(key: String, value) -> void:
+	var mark_dirty = false
+	if value != get_value(key):
+		mark_dirty = true # Needs to be set prior to set
+	data[key] = value
+	if mark_dirty:
+		save_game()
+
+func get_value(key: String, default = null):
+	return data.get(key, default)
+
+func remove_value(key: String) -> void:
+	if data.has(key):
+		data.erase(key)
+		save_game()
+
+func save_game() -> void:
+	var file := FileAccess.open(FILE_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(data))
+
+func load_game() -> Dictionary:
+	if not FileAccess.file_exists(FILE_PATH):
+		return {}
+	var file := FileAccess.open(FILE_PATH, FileAccess.READ)
+	return JSON.parse_string(file.get_as_text()) as Dictionary
