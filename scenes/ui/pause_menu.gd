acc2c8113baf610a8e8c1a82eba9763fe295d35f
Clicksurfer <amit.chameides@gmail.com>
2025-11-23 18:51:16 +1100

    Added fake functionality for audio button

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -12,19 +12,28 @@ var _sound_is_on:bool = true
 
 # Opens the popup, connecting up the provided button functionality.
 func open_popup(game_controller:GameController):
-    _game_controller = game_controller
-    show()
+	_game_controller = game_controller
+	_setup_buttons()
+	show()
+
+func _setup_buttons():
+	if !_toggle_audio_button.pressed.is_connected(_toggle_audio_button_clicked):
+		_toggle_audio_button.pressed.connect(_toggle_audio_button_clicked)
+	if !_retry_button.pressed.is_connected(_restart_button_clicked):
+		_retry_button.pressed.connect(_restart_button_clicked)
+	if !_home_button.pressed.is_connected(_quit_button_clicked):
+		_home_button.pressed.connect(_quit_button_clicked)
 
 func _toggle_audio_button_clicked():
-    # There is no sound in the game so this does nothing. For now we'll just change the icon.
-    _sound_is_on = !_sound_is_on
-    if (_sound_is_on):
-        _toggle_audio_button.icon = _audio_on_icon
-    else:
-        _toggle_audio_button.icon = _audio_off_icon
+	# There is no sound in the game so this does nothing. For now we'll just change the icon.
+	_sound_is_on = !_sound_is_on
+	if (_sound_is_on):
+		_toggle_audio_button.icon = _audio_on_icon
+	else:
+		_toggle_audio_button.icon = _audio_off_icon
 
 func _quit_button_clicked():
-    _game_controller.quit()
+	_game_controller.quit()
 
 func _restart_button_clicked():
-    _game_controller.restart()
+	_game_controller.restart()
